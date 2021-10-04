a93740109f4118947c62fee3f60425bea5e6b5f3
srishti-maurya <srishti22maurya@gmail.com>
2021-10-04 15:13:33 +0530

    formik form part 1

--- screens/home.js
+++ screens/home.js
@@ -11,6 +11,8 @@ import { globalStyles } from "../styles/global";
 import Card from "../shared/card";
 import { MaterialIcons } from "@expo/vector-icons";
 
+import ReviewForm from "../screens/reviewForms";
+
 export default function Home({ navigation }) {
   const [modalOpen, setModalOpen] = useState(false);
   const [reviews, setReviews] = useState([
@@ -44,7 +46,7 @@ export default function Home({ navigation }) {
             style={{ ...styles.modalToggle, ...styles.modalClose }}
             onPress={() => setModalOpen(false)}
           />
-          <Text>Hello from modal</Text>
+          <ReviewForm />
         </View>
       </Modal>
       <MaterialIcons
